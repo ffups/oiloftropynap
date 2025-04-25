fa8d3cb4952fc5b5b07042fb964181b9fa58ca0d
ffups <142579057+ffups@users.noreply.github.com>
2025-04-25 05:52:56 +0200

    clenaup frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,32 +1,9 @@
-"use client";
-
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 export default function Page() {
-  const [navbars, setNavbars] = useState<any[]>([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:1337/api/navbars')
-      .then(response => {
-        setNavbars(response.data.data);
-        console.log(response.data.data); // Add this line
-      })
-      .catch(error => console.error(error));
-  }, []);
-
   return (
     <div>
       <h1>Navbar Items</h1>
-      <ul>
-        {navbars.map(item => (
-          <li key={item.id}>
-            <a href={item.link} target="_blank" rel="noopener noreferrer">
-              {item.title}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <p>hello</p>
     </div>
   );
 }
