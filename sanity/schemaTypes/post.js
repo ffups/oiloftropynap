c2c3fb158e76836cef301850f732a49a5a9d06dd
ffups <142579057+ffups@users.noreply.github.com>
2025-04-27 02:09:43 +0200

    sanity fresh

---
+++ sanity/schemaTypes/post.js
@@ -0,0 +1,12 @@
+import { defineType, defineField } from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({ name: 'title', title: 'Title', type: 'string' }),
+    defineField({ name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title', maxLength: 96 } }),
+    defineField({ name: 'body', title: 'Body', type: 'text' }),
+  ],
+})
